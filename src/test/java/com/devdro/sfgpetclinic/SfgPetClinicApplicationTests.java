d66a4067ca46046422c5747fabddbd9d31665856
dev-dro <danilo.ro@protonmail.com>
2020-10-13 13:09:46 -0300

    Initial project add

---
+++ src/test/java/com/devdro/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdro.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
