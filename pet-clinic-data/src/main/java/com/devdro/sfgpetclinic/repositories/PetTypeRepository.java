692c301557f2a1d659a722a95c5e469f8d24efaf
dev-dro <danilo.ro@protonmail.com>
2021-02-01 19:40:17 -0300

    Added spring data jpa for petType.

--- pet-clinic-data/src/main/java/com/devdro/sfgpetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/devdro/sfgpetclinic/repositories/PetTypeRepository.java
@@ -1,8 +1,9 @@
 package com.devdro.sfgpetclinic.repositories;
 
+import com.devdro.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PetTypeRepository extends CrudRepository<PetTypeRepository, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/devdro/sfgpetclinic/services/springdatajpa/PetTypeSpringJpaService.java
@@ -0,0 +1,49 @@
+package com.devdro.sfgpetclinic.services.springdatajpa;
+
+import com.devdro.sfgpetclinic.model.PetType;
+import com.devdro.sfgpetclinic.repositories.PetTypeRepository;
+import com.devdro.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSpringJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSpringJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public Optional<PetType> findById(Long id) {
+        return petTypeRepository.findById(id);
+    }
+
+    @Override
+    public Optional<PetType> save(PetType object) {
+        return Optional.of(petTypeRepository.save(object));
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
