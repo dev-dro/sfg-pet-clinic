1cd24488359f0eacd0ebf99bfb6ddc9205343d1a
dev-dro <danilo.ro@protonmail.com>
2021-02-01 18:27:03 -0300

    Update owner with jpa mappings.

--- pet-clinic-data/src/main/java/com/devdro/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/devdro/sfgpetclinic/model/Pet.java
@@ -1,12 +1,28 @@
 package com.devdro.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birthday")
     private LocalDate birthday;
 
     public PetType getPetType() {
